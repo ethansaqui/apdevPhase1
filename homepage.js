3524bc02798bf387d66ea80826fcd5d8cebf6201
ethansaqui <104416692+ethansaqui@users.noreply.github.com>
2022-05-04 16:30:43 +0800

    Added comment box
    pain

--- homepage.js
+++ homepage.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
-    
+    $(".reply-container").addClass("display-replies");
+    var clicked = false;
+    $(".reply-button").on("click", function() {
+        console.log("clicked");
+        clicked = !clicked;
+        
+        $(this).parents(".comment-box").toggleClass("extend")
+        $(this).parents(".comment-box").find(".reply-container").toggleClass("display-replies");
+    })
+
+    var replyButtons = $(".reply-button");
+
+
+    $(".reply-button").hover(function() {
+        $(this).addClass("rotate-button");
+    },
+    function() {
+        if(clicked == false) {
+            $(this).removeClass("rotate-button");
+        }
+    })
+
+
+
 }) 
 
 

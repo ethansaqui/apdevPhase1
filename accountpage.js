e91efecd3f5f5bf8f314e80d88cdd37752de781d
Gabe Piano <43084571+Poije@users.noreply.github.com>
2022-05-18 00:23:15 +0800

    Upload Pop Up Added

---
+++ accountpage.js
@@ -0,0 +1,6 @@
+function toggleUpload(){
+    document.getElementById("uploadpopup").classList.toggle("active");
+}
+function cancelUpload(){
+    document.getElementById("uploadpopup").classList.toggle("cancel");
+}

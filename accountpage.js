b57f312ea426d7a897b441971586cd8f6a3faa5c
Gabe Piano <43084571+Poije@users.noreply.github.com>
2022-05-18 16:35:27 +0800

    Delete Post

--- accountpage.js
+++ accountpage.js
@@ -1,8 +1,35 @@
 function toggleUpload(){
     document.getElementById("uploadpopup").classList.toggle("active");
 }
-function cancelUpload(){
-    document.getElementById("uploadpopup").classList.toggle("cancel");
+function DeleteWork(clicked_id){
+    var UsedId = clicked_id;
+    console.log(UsedId)
+    switch(UsedId)
+    {
+        case 'Delete1':
+            document.getElementById("Art1").src = "Unavailable.png";
+            document.getElementById("Art1").style.objectFit = "contain";
+            break;
+        case 'Delete2':
+            document.getElementById("Art2").src = "Unavailable.png";
+            document.getElementById("Art2").style.objectFit = "contain";
+            break;
+        case 'Delete3':
+            document.getElementById("Art3").src = "Unavailable.png";
+            document.getElementById("Art3").style.objectFit = "contain";
+            break;
+        case 'Delete4':
+            document.getElementById("Art4").src = "Unavailable.png";
+            document.getElementById("Art4").style.objectFit = "contain";
+            break;
+         case 'Delete5':
+            document.getElementById("Art5").src = "Unavailable.png";
+            document.getElementById("Art5").style.objectFit = "contain";
+            break;
+        default:
+            console.log('Error');
+            break;
+    }
 }
 function Follow(){
     document.getElementById("followercount").innerHTML = "1 ";

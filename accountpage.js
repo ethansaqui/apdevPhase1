85f76f6b15028dd88b1ffc867280535c7d495fca
Gabe Piano <43084571+Poije@users.noreply.github.com>
2022-05-18 17:37:21 +0800

    Delete accountpage.js
    something wrong

--- accountpage.js
+++
@@ -1,6 +0,0 @@
-function toggleUpload(){
-    document.getElementById("uploadpopup").classList.toggle("active");
-}
-function Follow(){
-    document.getElementById("followercount").innerHTML = "1 ";
-}

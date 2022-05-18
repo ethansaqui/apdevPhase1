2a9bb7196a0a4356f86d5e109d98a33e92dfc9cc
Gabe Piano <43084571+Poije@users.noreply.github.com>
2022-05-18 17:41:15 +0800

    Revert "Delete accountpage.js"
    This reverts commit 85f76f6b15028dd88b1ffc867280535c7d495fca.

---
+++ accountpage.js
@@ -0,0 +1,6 @@
+function toggleUpload(){
+    document.getElementById("uploadpopup").classList.toggle("active");
+}
+function Follow(){
+    document.getElementById("followercount").innerHTML = "1 ";
+}

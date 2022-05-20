f3673043e871254771febda71def30eb7dd8f4d0
ethansaqui <104416692+ethansaqui@users.noreply.github.com>
2022-05-20 14:14:31 +0800

    Made nested comments

--- homepage.js
+++ homepage.js
@@ -1,13 +1,51 @@
 $(document).ready(function() {
+    // Comment Functions
+    const user = "Anya";
+    
+    // Hide Show Replies for comments without replies
+    $(".comment").each(function (){
+        let c = $(this);
+        
+        var containers = c.next(".comment-container");
+        if (containers.length == 0) {
+            c.find(".show-replies").css("display", "none");
+        }
+    })
 
-   
+    // Give only Anya the power to delete and edit her comments
+    $(".comment-text h3").each(function () {
+        var cText = $(this);
+        if(cText.text() != user) {
+            console.log("in")
+            console.log(cText.parent());
+            cText.parents(".comment").find(".user").css("display", "none")
+        }
+    })
+
+    
+    var replyHidden = false;
+    // change all show reply to hide reply
+    $(".show-replies").text("Hide Replies")
+
+    $(".show-replies").on("click", function() {
+        var reply = $(this).closest(".comment").next(".comment-container");
+        if(replyHidden) {
+            reply.css("display", "block");
+            replyHidden = false;
+            $(this).text("Hide Replies");
+            return;
+        }
+        reply.css("display", "none");
+        replyHidden = true;
+        $(this).text("Show Replies");
+    })
 
     // Popup Modal Image Functions
     $(".posts-wrapper .popup-image span").on("click", () => {
         $(".posts-wrapper .popup-image").css("display", "none");
     })
 
-    $(".posts-wrapper .post-image").on("click", function() {
+    $(".posts-wrapper .post-image").on("click", function() { // Update the Image, Caption, and Artist of a Modal
         var imgsrc = $(this).attr('src');
         
         const img = new Image();
@@ -17,15 +55,13 @@ $(document).ready(function() {
 
         var caption = $(this).parents(".image-container").find(".caption").text();
         $(".posts-wrapper .popup-image .caption").text(caption);
+
         var artist = $(this).parents(".image-container").find(".artist").text();
         $(".posts-wrapper .popup-image .artist").text(artist);
 
         var pfpsrc = $(this).parents(".parent").find("#profile-picture img").attr('src');
         $(".popup-image #profile-picture img").attr('src', pfpsrc);
 
-     
-
-        console.log($(".posts-wrapper .popup-image .popup").css("width"));
         $(".posts-wrapper .popup-image").css("display", "block");
         $(".popup-image .popup").attr('src', imgsrc);
     })
@@ -33,12 +69,9 @@ $(document).ready(function() {
     $("#profile-picture img").each(function() { 
         var imgsrc = $(this).attr('src');
         const img = new Image();
-        console.log(imgsrc);
         img.src = imgsrc;
         if(img.width > img.height) {
             $(this).width = img.height;
-            console.log(img.width);
-            console.log(img.height);
         }
     })
 })
@@ -46,4 +79,6 @@ $(document).ready(function() {
 function toggleUpload(){
     document.getElementById("uploadpopup").classList.toggle("active");
 }
+
+
 

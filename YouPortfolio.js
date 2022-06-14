d1bf846462efe69f488b17c746a92f502ae728eb
Gabe Piano <43084571+Poije@users.noreply.github.com>
2022-06-14 23:05:35 +0800

    Added all to Local Host

--- YouPortfolio.js
+++ YouPortfolio.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = new express();
-
 const mongoose = require ('mongoose');
-mongoose.connect('mongodb://localhost/YouPortfolioDB',
-{useNewURLParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/youPortfolioDB',
+{useNewURLParser: true, useUnifiedTopology: true}); //create database connection
 
 const fileUpload = require('express-fileupload');
+
 
 app.use(express.static('public'));
 
 app.get('/',function(req,res){
     res.sendFile(__dirname + '\\' + 'login.html');
+});
+
+app.get('/homepage',function(req,res){
+    res.sendFile(__dirname + '\\' + 'homepage.html');
+});
+
+app.get('/register',function(req,res){
+    res.sendFile(__dirname + '\\' + 'Register.html');
+});
+
+app.get('/account',function(req,res){
+    res.sendFile(__dirname + '\\' + 'accountpage.html');
 });
 
 var server = app.listen(3000,function(){
